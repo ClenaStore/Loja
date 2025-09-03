e7bd2426b98d075ee471aa0675077b3508ed404c
clena <storeclena@gmail.com>
2025-09-03 15:53:09 -0300

    Update create_preference.js

--- api/create_preference.js
+++ api/create_preference.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       const response = await fetch("https://api.mercadopago.com/checkout/preferences", {
         method: "POST",
         headers: {
-          "Authorization": `Bearer ${process.env.MP_ACCESS_TOKEN}`,
+          "Authorization": `Bearer ${process.env.MP_ACCESS_TOKEN}`, // 👉 precisa ser o TEST_ACCESS_TOKEN
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
@@ -33,10 +33,7 @@ export default async function handler(req, res) {
       if (data.id) {
         res.status(200).json({ id: data.id });
       } else {
-        res.status(400).json({
-          error: "Erro do Mercado Pago",
-          details: data // aqui devolve a mensagem de erro completa
-        });
+        res.status(response.status).json({ error: "Erro do Mercado Pago", details: data });
       }
     } catch (error) {
       res.status(500).json({ error: "Erro interno", details: error.message });
